96e9a98bcacf2435f7aa7f8e4dc3229c1853b627
JimmyMcBride <mcbride967@gmail.com>
2019-05-10 16:30:46 -0500

    fixed token

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
-const token = 'NTc2NDk1MTU5NjI2MTcwMzgx.XNXWMw.cG8umYMTB4h0sYOkJS3'
+const token = 'NTc2NDk1MTU5NjI2MTcwMzgx.XNXWMw.cG8umYMTB4h0sYOkJS3XCSlckgo'
 
-bot.on('ready', () => {
-    console.log('Your bot is online.')
-})
+bot.on('ready')
 
 bot.on('message', msg => {
     if (msg.content === "Hello, Fire Mage!"){
