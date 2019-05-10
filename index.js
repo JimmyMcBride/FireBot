8035d2e0057a699c8e097a8a593c292c051058f1
JimmyMcBride <mcbride967@gmail.com>
2019-05-10 17:15:18 -0500

    ;

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-const token = 'NTc2NDk1MTU5NjI2MTcwMzgx.XNXWMw.cG8umYMTB4h0sYOkJS3XCSlckgo'
+const token = 'NTc2NDk1MTU5NjI2MTcwMzgx.XNXWMw.cG8umYMTB4h0sYOkJS3XCSlckgo';
 
-bot.on('ready')
 
-bot.on('message', msg => {
-    if (msg.content === "Hello, Fire Mage!"){
-        msg.reply('Hello, friend!')
-    }
-})
-
-bot.login(token)
+bot.login(token);
