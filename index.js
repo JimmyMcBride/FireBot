953fb5dce2d053ba6ea94f7c64154226307c9146
JimmyMcBride <mcbride967@gmail.com>
2019-08-07 17:19:34 -0500

    touched up code

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const Discord = require('discord.js')
+const bot = new Discord.Client()
 
-const token = 'NTc2NDk1MTU5NjI2MTcwMzgx.XNXWMw.cG8umYMTB4h0sYOkJS3XCSlckgo';
+const config = require('./config.json')
+const token = config.token
+
+bot.on('ready', () => {
+    console.log('Fire Mage is online!')
+})
+
+bot.on('message', msg => {
+    let args = msg.content.substring(config.prefix.length).split(' ')
+
+    let randomNumber = Math.floor(Math.random() * 100) + 1
+    let emoji = () => {
+        if (randomNumber > 1 && randomNumber < 25) {
+            return '☠️'
+        } else if (randomNumber > 26 && randomNumber < 50) {
+            return '💩'
+        }
+        else if (randomNumber > 51 && randomNumber < 75) {
+            return '😏'
+        } else if (randomNumber > 76 && randomNumber < 100) {
+            return '🔥'
+        }
+    }
+
+    if (msg.content === 'Introduce yourself, Fire Mage.'){
+        msg.channel.send(`I'm a discord bot built by the **ALL MIGHTY** 🔥 Fire Ninja 🔥.
+You can type **!roll** to get a random number between 1-100.
+Also, try typing **!ping** or **!marco** to play a game!`)
+    }
+
+    switch(args[0]){
+        case 'ping':
+            msg.reply('pong!')
+        break
+
+        case 'marco':
+            msg.reply('polo!')
+        break
+
+        case 'roll':
+            msg.reply(`rolled a ${randomNumber}! ${emoji()}`)
+        break
+
+        case 'info':
+    }
 
 
-bot.login(token);
+})
+
+bot.login(token)
